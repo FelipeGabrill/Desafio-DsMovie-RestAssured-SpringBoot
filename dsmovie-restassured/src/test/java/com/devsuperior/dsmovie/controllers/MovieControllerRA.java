7b03e93518707684c41541df5893f2c96454cdbb
FelipeGabrill <felipegabrielodss@gmail.com>
2025-02-23 19:06:34 -0300

    findAllShouldReturnOkWhenMovieNoArgumentsGiven, findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty

--- dsmovie-restassured/src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
+++ dsmovie-restassured/src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -1,16 +1,68 @@
 package com.devsuperior.dsmovie.controllers;
 
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
 import org.json.JSONException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import com.devsuperior.dsmovie.tests.TokenUtil;
 
 public class MovieControllerRA {
 	
+	private String clientUsername, clientPassword, adminUsername, adminPassword;
+	private String clientToken, adminToken, invalidToken;
+	private String movieTitle;
+	private Long existingMovieId, nonExistingMovieId;
+	
+	@BeforeEach
+	void setUp() throws JSONException {
+		baseURI = "http://localhost:8080";
+		
+		existingMovieId = 1L;
+		nonExistingMovieId = 1000L;
+		
+		movieTitle = "matrix";
+		
+		clientUsername = "ana@gmail.com";
+		clientPassword = "123456";
+		
+		adminUsername = "maria@gmail.com";
+		adminPassword = "123456";
+		
+		clientToken = TokenUtil.obtainAccessToken(clientUsername, clientPassword);
+		adminToken = TokenUtil.obtainAccessToken(adminUsername, adminPassword);
+		invalidToken = adminToken + "xpto";
+		
+	}
+	
 	@Test
 	public void findAllShouldReturnOkWhenMovieNoArgumentsGiven() {
+		
+		given() 
+			.get("/movies")
+		.then()
+			.statusCode(200);
 	}
 	
 	@Test
 	public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {		
+		given() 
+			.get("/movies?size=25&title={movieTitle}", movieTitle)
+		.then()
+			.statusCode(200)
+			.body("content.id[0]", is(4))
+			.body("content.title[0]", equalTo("Matrix Resurrections"))
+			.body("content.score[0]", is(0.0F))
+			.body("content.count[0]", is(0))
+			.body("content.image[0]", equalTo("https://www.themoviedb.org/t/p/w533_and_h300_bestv2/hv7o3VgfsairBoQFAawgaQ4cR1m.jpg"));
+		
+		
 	}
 	
 	@Test
